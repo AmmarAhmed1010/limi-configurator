2b01a0011ebbf0408acfd898c4979106deef70e3
Ammar Ahmed <ammar7298@gmail.com>
2025-10-30 21:15:00 +0500

    changes

--- src/app/dashboard/components/SlideManagement.jsx
+++ src/app/dashboard/components/SlideManagement.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { buildApi1Url } from '../../../config/api.config';
+import { buildApi1Url } from '../../../../config/api.config';
 import { useSelector, useDispatch } from "react-redux";
 import {
   FaPlus,
