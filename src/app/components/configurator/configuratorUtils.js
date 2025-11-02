336137ef51528cb6a83190d63fbc69a169ac8b5f
Ammar Ahmed <ammar7298@gmail.com>
2025-11-02 11:43:24 +0500

    changes

--- src/app/components/configurator/configuratorUtils.js
+++ src/app/components/configurator/configuratorUtils.js
@@ -164,7 +164,7 @@ export const getDefaultDesigns = (amount) => {
  * @param {string} message - The message to send
  */
 export const sendMessageToPlayCanvas = (message) => {
-  //console.log("Sending message to PlayCanvas iframe:", message);
+  console.log("Sending message to PlayCanvas iframe:", message);
   const iframe = document.getElementById("playcanvas-app");
   if (iframe && iframe.contentWindow) {
     iframe.contentWindow.postMessage(message, "*");
@@ -203,7 +203,7 @@ export const sendMessagesForDesign = (designName, idOrIds) => {
     if (assignment.systemType === "bar") {
       sendMessageToPlayCanvas("barextra");
     }else{
-      sendMessageToPlayCanvas("Nobars");
+      // sendMessageToPlayCanvas("Nobars");
     }
     sendMessageToPlayCanvas(`cable_${id}`);
     sendMessageToPlayCanvas(
