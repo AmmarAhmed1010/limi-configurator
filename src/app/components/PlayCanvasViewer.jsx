d1a2e6844d658cdea6e18d22b50ec955e7be18f7
Ammar Ahmed <ammar7298@gmail.com>
2026-02-19 14:47:05 +0500

    fixes

--- src/app/components/PlayCanvasViewer.jsx
+++ src/app/components/PlayCanvasViewer.jsx
@@ -366,7 +366,8 @@ const PlayCanvasViewer = ({
         id="playcanvas-app"
         title="3D Configurator Preview"
         // https://configurator.limilighting
-        src="https://playcanv.as/e/p/7c2273a2/"
+        // src="https://playcanv.as/e/p/7c2273a2/"
+        src='https://limi-configurator-dev.vercel.app/'
         // src="https://limi-conf.vercel.app/"
         // src='https://limi-configurator-temp.vercel.app/'
         // src='https://limiliveconfigurator.vercel.app/'
